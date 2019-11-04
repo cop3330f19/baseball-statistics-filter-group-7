f2d77b9692ce68983404a4053fe472aef688a776
trilltrev <48462041+trilltrev@users.noreply.github.com>
2019-11-04 18:27:46 -0500

    Update FilterDriver.cpp

--- FilterDriver.cpp
+++ FilterDriver.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include "BaseballStatistic.h"
+#include "StringHelper.h"
+#include "Date.h"
 
+
+
+using namespace std;
+
+
+void swap(vector<BaseballStatistic>&, int int);
+void sort(vector<BaseballStatistic>&);
+int  search(vecotr<BaseballStatistic>, string, string);
+void loadFile(vector<BaseballStatistic>&);    
+
+int main(){
+    
+   vector<BaseballStatistic> test1;
+  string temp ("R");
+  char p[temp.length()];
+  
+  p[0] = temp[0]
